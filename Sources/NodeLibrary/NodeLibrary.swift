6a95500ddd4bab06169ccf4a8e99183c2888e95b
Jure <jure.mb@me.com>
2021-11-05 12:08:37 +0100

    Test commit on Dev

--- Sources/NodeLibrary/NodeLibrary.swift
+++ Sources/NodeLibrary/NodeLibrary.swift
@@ -1,5 +1,6 @@
 public struct NodeLibrary {
     public private(set) var text = "Hello, World!"
+    public private(set) var testIsTrue = true
     static func makeDomain() -> Domain<NoGroups,NoShape> {
         return Domain()
     }
